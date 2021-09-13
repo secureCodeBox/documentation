378f130152d974856ed7564c22923c970e795ed2
Daniel Patanin <daniel.patanin@iteratec.com>
2021-09-13 14:50:48 +0200

    adjust to color mode

--- src/layouts/Sections.tsx
+++ src/layouts/Sections.tsx
@@ -2,23 +2,29 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+import useThemeContext from "@theme/hooks/useThemeContext";
 import React from "react";
 
-const Sections = ({ children }: { children: React.ReactNode }) => (
-  <div>
-    {React.Children.map(children, (child, idx) => (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          backgroundColor: idx % 2 == 1 ? "#E2E2E2" : "white",
-          padding: "2rem 4rem",
-        }}
-      >
-        <div style={{ maxWidth: "1140px" }}>{child}</div>
-      </div>
-    ))}
-  </div>
-);
+const Sections = ({ children }: { children: React.ReactNode }) => {
+  const { isDarkTheme } = useThemeContext(),
+    bgColor = isDarkTheme ? "#5c5c5c" : "#E2E2E2";
+
+  return (
+    <div>
+      {React.Children.map(children, (child, idx) => (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            backgroundColor: idx % 2 == 1 ? bgColor : "",
+            padding: "2rem 4rem",
+          }}
+        >
+          <div style={{ maxWidth: "1140px" }}>{child}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 export default Sections;
