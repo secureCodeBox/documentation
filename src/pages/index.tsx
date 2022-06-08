e65fb8011aab409388bb5cd51ecae00a0dad1978
Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>
2022-06-08 17:52:14 +0200

    Corrected comment with the new name of the theme function
    useThemeContext() --> useColorMode()
    
    Signed-off-by: Ilyes Ben Dlala <ilyes.bendlala@iteratec.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,7 +45,7 @@ const HookIntegrations = () => {
   );
 };
 
-//* In order to use useThemeContext() on the homepage, the main content is separated from the default export due to how "@theme/Layout" works
+//* In order to use useColorMode() on the homepage, the main content is separated from the default export due to how "@theme/Layout" works
 function HomePage() {
   const { isDarkTheme } = useColorMode();
 
