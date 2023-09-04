9f6d9ff5a4cc3fa66d842d8e9f81497d0742d37c
Heiko Kiesel <heiko.kiesel@iteratec.com>
2023-09-04 14:07:03 +0200

    Delete docusaurus config files as already moved to monorepo
    Signed-off-by: Heiko Kiesel <heiko.kiesel@iteratec.com>

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: the secureCodeBox authors
-//
-// SPDX-License-Identifier: Apache-2.0
-
-module.exports = {
-  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
-};
